01c24dd92f14f265a95d672c49ab7bcd5afd8444
Shubham Kumar Jha <skjha832@gmail.com>
2019-04-27 22:42:33 +0530

    To delete some files

---
+++ Gallery/app/src/main/java/app/com/shubham/gallery/api/APIClient.java
@@ -0,0 +1,25 @@
+package app.com.shubham.gallery.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = null;
+
+    static Retrofit getClient(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
